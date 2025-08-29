8a57f4d9864830ff85aa3f6c269d8d72e5f66769
fedo-code <polash.gohain14@gmail.com>
2025-08-29 18:09:44 +0530

    Fix API base URL for production

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,25 +1,15 @@
-//import axios from 'axios';
-//const instance = axios.create({
-  //baseURL: 'http://localhost:5000/api',
- 
-//});
-//instance.interceptors.request.use((config) => {
-  //const token = localStorage.getItem('token');
- // if (token) config.headers.Authorization = `Bearer ${token}`;
-  //return config;
-//});
-
-//export default instance;
-
-// utils/axiosInstance.js
 import axios from 'axios';
 
+const baseURL = process.env.NODE_ENV === 'production' 
+  ? 'https://skillswap-co6l.onrender.com/api' 
+  : 'http://localhost:5000/api';
+
 const instance = axios.create({
-  baseURL: 'http://localhost:5000/api', // ✅ tu jo use karta ha wo daal
+  baseURL: baseURL,
 });
 
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // ✅ token localStorage me hona chahiye
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
@@ -27,5 +17,3 @@ instance.interceptors.request.use((config) => {
 });
 
 export default instance;
-
- 

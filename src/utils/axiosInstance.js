f436006fc40f6eae21287394bc1f7c0dd3b3c99c
fedo-code <polash.gohain14@gmail.com>
2025-08-29 18:25:41 +0530

    undo changes

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,19 +1,31 @@
+//import axios from 'axios';
+//const instance = axios.create({
+  //baseURL: 'http://localhost:5000/api',
+ 
+//});
+//instance.interceptors.request.use((config) => {
+  //const token = localStorage.getItem('token');
+ // if (token) config.headers.Authorization = `Bearer ${token}`;
+  //return config;
+//});
+
+//export default instance;
+
+// utils/axiosInstance.js
 import axios from 'axios';
 
-const baseURL = process.env.NODE_ENV === 'production' 
-  ? 'https://skillswap-co6l.onrender.com/api' 
-  : 'http://localhost:5000/api';
-
 const instance = axios.create({
-  baseURL: baseURL,
+  baseURL: 'http://localhost:5000/api', // ✅ tu jo use karta ha wo daal
 });
 
 instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
+  const token = localStorage.getItem('token'); // token localStorage me hona chahiye
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
   return config;
 });
+ 
+export default instance; 
 
-export default instance;
+ 
